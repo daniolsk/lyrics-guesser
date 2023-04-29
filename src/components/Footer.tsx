44c4db70bd305c55784e9a27ed3d9882448096f8
daniolsk <danio.skowron@gmail.com>
2023-04-29 16:45:58 +0200

    change footer font weight

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function Footer() {
-	return <footer className="bg-black bg-opacity-40 p-4 text-center text-sm text-white">Made with ❤️ by Daniel Skowron</footer>;
+	return (
+		<footer className="bg-black bg-opacity-40 p-4 text-center text-sm font-semibold text-white">Made with ❤️ by Daniel Skowron</footer>
+	);
 }
 
 export default Footer;
