e216e71b248db611df332e367718f62afc67dd4f
daniolsk <danio.skowron@gmail.com>
2023-05-09 16:24:03 +0200

    change some styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -154,8 +154,10 @@ export default function Home() {
 							</button>
 						) : (
 							<div className="flex flex-col text-center text-2xl">
-								<div className="mb-1 font-semibold">Other gamemodes:</div>
-								<div className="mb-6 text-sm font-semibold text-gray-300">(songs are generated based on your Spotify)</div>
+								<div className="mb-1 text-2xl font-semibold md:text-3xl">Other gamemodes:</div>
+								<div className="mb-6 text-sm font-semibold text-gray-300 md:text-base">
+									(songs are generated based on your Spotify)
+								</div>
 								<div className="grid grid-cols-1 gap-4 md:grid-cols-3">
 									<div className="p-4">
 										<div className="mb-2 flex items-center justify-center text-xl font-semibold">
