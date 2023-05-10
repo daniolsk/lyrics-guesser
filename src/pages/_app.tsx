e14813de93e1feac9155566e82dae9c466f1ef02
daniolsk <danio.skowron@gmail.com>
2023-05-10 16:33:45 +0200

    remove useless console logs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,6 @@ import { SessionProvider } from 'next-auth/react';
 import Router from 'next/router';
 import Script from 'next/script';
 
-Router.events.on('routeChangeStart', () => console.log('1'));
-Router.events.on('routeChangeComplete', () => console.log('2'));
-Router.events.on('routeChangeError', () => console.log('3'));
-
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
 		<SessionProvider session={session}>
