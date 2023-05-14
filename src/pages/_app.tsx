f4e4962fb45d5b9a1d30f9eae58fd79784f5a075
daniolsk <danio.skowron@gmail.com>
2023-05-14 12:34:49 +0200

    fixed google tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,49 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import Router from 'next/router';
 import Script from 'next/script';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '../utils/gtag';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url);
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
 	return (
-		<SessionProvider session={session}>
-			<Component {...pageProps} />
-		</SessionProvider>
+		<>
+			<Head>
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3539636730894037"
+					crossOrigin="anonymous"
+				></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+					}}
+				/>
+			</Head>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
+			<SessionProvider session={session}>
+				<Component {...pageProps} />
+			</SessionProvider>
+		</>
 	);
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,44 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import { useEffect } from 'react';
-import * as gtag from '../utils/gtag';
 
 export default function Document() {
-	const router = useRouter();
-	useEffect(() => {
-		const handleRouteChange = (url: string) => {
-			gtag.pageview(url);
-		};
-		router.events.on('routeChangeComplete', handleRouteChange);
-		return () => {
-			router.events.off('routeChangeComplete', handleRouteChange);
-		};
-	}, [router.events]);
-
 	return (
 		<Html lang="en">
-			<Head>
-				<script
-					async
-					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3539636730894037"
-					crossOrigin="anonymous"
-				></script>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-					}}
-				/>
-			</Head>
-			<Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
+			<Head />
 			<body>
 				<Main />
 				<NextScript />
