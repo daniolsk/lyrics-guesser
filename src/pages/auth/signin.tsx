eb9c2ecbadaed5fa6026414fce9d6c659ec34ab8
daniolsk <danio.skowron@gmail.com>
2023-06-27 13:22:45 +0200

    disabled logging in

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -2,6 +2,7 @@ import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'nex
 import { getProviders, signIn } from 'next-auth/react';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../api/auth/[...nextauth]';
+import Link from 'next/link';
 
 export default function SignIn({ providers, error }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	return (
@@ -10,6 +11,7 @@ export default function SignIn({ providers, error }: InferGetServerSidePropsType
 			{Object.values(providers).map((provider) => (
 				<div key={provider.name}>
 					<button
+						disabled
 						className="my-4 cursor-pointer border-2 border-white bg-gray-1000 px-4 py-2 text-lg font-semibold hover:enabled:bg-white hover:enabled:text-black disabled:border-gray-500 disabled:text-gray-500 md:px-5 md:py-3 md:text-xl"
 						onClick={() => signIn(provider.id)}
 					>
@@ -17,6 +19,12 @@ export default function SignIn({ providers, error }: InferGetServerSidePropsType
 					</button>
 				</div>
 			))}
+			<div className="italic text-gray-400">
+				*currently not aviable{' '}
+				<Link className="underline" href="/">
+					(go back)
+				</Link>
+			</div>
 			{error ? <div className="mt-2 text-2xl font-semibold text-red-600">Something went wrong - try again later</div> : ''}
 		</div>
 	);
